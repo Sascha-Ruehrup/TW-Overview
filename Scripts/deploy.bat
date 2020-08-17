6d34e6fceef216417ff69606232476e3f9193c2d
Sascha <sascha.ruehrup@gmail.com>
2020-08-18 00:58:56 +0200

    added deploy.bat

---
+++ Scripts/deploy.bat
@@ -0,0 +1,16 @@
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:sascha-ruehrup/tw-overview.git master:gh-pages
+
+cd -
